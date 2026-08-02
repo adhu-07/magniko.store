01b303072b8267e8e6863acb0125029d5924774c
Adithyan Satheeshkumar <adithyan20060707@gmail.com>
2026-08-02 18:49:11 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './', // Ensures relative assets work on GitHub Pages (https://adhu-07.github.io/magniko.store/)
-})
